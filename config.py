f622e818333b877d0bdf1abe8689ae749b77e428
chanmantze <154297141+chanmantze@users.noreply.github.com>
2023-12-19 11:44:18 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cxweioy40rib.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "chanmantze-employee"
+customregion = "us-east-1"
+
